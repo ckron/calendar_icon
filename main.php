582bec766c83fbbb993baaaf2cb6341c3a6bfdc3
ckron <7393106+ckron@users.noreply.github.com>
2020-05-07 12:15:44 +0900

    make look the same layout for main.php and index html

--- main.php
+++ main.php
@@ -7,18 +7,17 @@ $note = htmlspecialchars($_GET['note'], ENT_QUOTES, "UTF-8");
 ?>
 <title><?php echo $yotei?></title>
 
-
 <link rel="apple-touch-icon" href=<?php echo "icon/icon_$month_$day.jpg"?>>
 
 <body onLoad="pDate()">
 <form action="main.php" method="GET" name="myFORM">
-<input type="text" name="yotei" size="30" maxlength="20" value=<?php echo $yotei?>><br>
-月を入力<br>
-<input type="text" name="month" size="6" maxlength="4" value=<?php echo $month?>><br>
-日を入力<br>
-<input type="text" name="day" size="6" maxlength="4" value=<?php echo $day?>><br>
-メモ<br>
-<textarea name="note"><?php echo $note?></textarea><br>
-<input type="submit" value="送信"><br>
+<p>
+予定登録(*は必須)<br>
+<input type="text" name="yotei" size="100%" maxlength="20" placeholder="* 予定名を入力" value=<?php echo $yotei?> required><br>
+<input type="text" name="month" size="100%" maxlength="4" placeholder="* 月を入力" value=<?php echo $month?> required><br>
+<input type="text" name="day" size="100%" maxlength="4" placeholder="* 日を入力" value=<?php echo $day?> required><br>
+<textarea name="note" placeholder="メモを入力"><?php echo $note?></textarea><br>
+<input type="submit" value="送信">
+</p>
 </form>
 </body>
