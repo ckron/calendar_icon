917f2a060436015bd5cff7fb0d79d78897713c3e
ckron <7393106+ckron@users.noreply.github.com>
2023-08-23 13:21:26 +0900

    :dvd: change programing language(php -> js)

---
+++ main.js
@@ -0,0 +1,13 @@
+
+let url = new URL(window.location.href);
+
+let params = url.searchParams;
+
+let yotei = params.get("yotei");
+let month = params.get("month");
+let day = params.get("day");
+let note = params.get("note");
+
+document.title = yotei;
+document.getElementById("icon-link").href = "icon/icon_" + month + "_" + day + ".jpg";
+document.getElementById("memo-area").value = note;
