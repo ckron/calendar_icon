67aff8ff7016218ddd52c64361eb17b485ccbcaf
ckron <7393106+ckron@users.noreply.github.com>
2025-02-17 17:07:28 +0900

    :dvd: change to create dynamic icon

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const FONT_SIZE = 60;
 
 let url = new URL(window.location.href);
 
@@ -8,11 +9,37 @@ let month = params.get("month");
 let day = params.get("day");
 let note = params.get("note");
 
+const canvas = document.querySelector("canvas");
+let width = canvas.width;
+let height = canvas.height;
+
+const ctx = canvas.getContext("2d");
+ctx.fillStyle = "black";
+ctx.font = "bold " + FONT_SIZE + "px tahoma";
+ctx.fillRect(0, 0, width, height);
+
+ctx.fillStyle = "white";
+ctx.fillRect(20, 20, width-40, height-40);
+
+ctx.fillStyle = "black";
+ctx.textAlign = "center";
+ctx.fillText(("00" + month).slice(-2) + "/" + ("00" + day).slice(-2),
+            width/2.0,
+            height/2.0,
+            width);
+
+let link = document.createElement("link");
+link.type = "image/icon";
+link.rel = "custom icon";
+link.href = canvas.toDataURL("image/icon");
+
+console.log(link.href);
+
 if(yotei !== null) {
   document.title = yotei;
 }
 if(month !== null && day !== null) {
-  document.getElementById("icon-link").href = "icon/icon_" + month + "_" + day + ".jpg";
+  document.getElementById("icon-link").href = link.href;
 }
 document.getElementById("yotei-area").value = yotei;
 document.getElementById("month-area").value = month;
