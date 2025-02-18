f89c2bc8ae022a4245a47389aecbbdb1c7781dac
ckron <7393106+ckron@users.noreply.github.com>
2025-02-19 00:45:50 +0900

    :dvd: add createFillRoundRect()

--- main.js
+++ main.js
@@ -1,6 +1,27 @@
 const FONT_MONTH_SIZE = 60;
 const FONT_DAY_SIZE = 150;
 
+const dark_gray = "rgb(50, 50, 50)"
+
+function createFillRoundRect(ctx, x, y, width, height, radius)
+{
+  const w = width;
+  const h = height;
+  const r = radius;
+  ctx.beginPath();
+  ctx.moveTo(x + r, y);
+  ctx.lineTo(x + w - r, y);
+  ctx.arc(x + w - r, y + r, r, Math.PI * (3.0/2.0), 0, false);
+  ctx.lineTo(x + w, y + h - r);
+  ctx.arc(x + w - r, y + h - r, r, 0, Math.PI * (1.0/2.0), false);
+  ctx.lineTo(x + r, y + h);
+  ctx.arc(x + r, y + h - r, r, Math.PI * (1.0/2.0), Math.PI, false);
+  ctx.lineTo(x, y + r);
+  ctx.arc(x + r, y + r, r, Math.PI, Math.PI * (3.0/2.0), false);
+  ctx.closePath();
+  ctx.fill();
+}
+
 let url = new URL(window.location.href);
 
 let params = url.searchParams;
@@ -15,13 +36,13 @@ let width = canvas.width;
 let height = canvas.height;
 
 const ctx = canvas.getContext("2d");
-ctx.fillStyle = "black";
+ctx.fillStyle = dark_gray;
 ctx.fillRect(0, 0, width, height);
 
 ctx.fillStyle = "white";
-ctx.fillRect(20, 20, width-40, height-40);
+createFillRoundRect(ctx, 20, 20, width-40, height-40, 20);
 
-ctx.fillStyle = "black";
+ctx.fillStyle = dark_gray;
 ctx.font = "bold " + FONT_MONTH_SIZE + "px tahoma";
 ctx.textAlign = "center";
 ctx.fillText(("00" + month).slice(-2) + "/", 100, 80, width);
